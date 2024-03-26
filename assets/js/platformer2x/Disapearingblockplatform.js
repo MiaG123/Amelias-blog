aff2b1939239d0d9da3b3f7e44858820a24e14dd
miag123 <miagiacosa@outlook.com>
2024-03-26 20:51:38 +0000

    update

--- assets/js/platformer2x/Disapearingblockplatform.js
+++ assets/js/platformer2x/Disapearingblockplatform.js
@@ -1,16 +1,29 @@
 import BlockPlatform from './BlockPlatform.js';
 import GameEnv from './GameEnv.js';
 export class Disappearingblockplatform extends BlockPlatform {
-    constructor(x, y, width, height, canvas, duration, delay) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.canvas = canvas;
-        this.duration = duration; // Duration of disappearance in milliseconds
-        this.delay = delay; // Delay before reappearing in milliseconds
-        this.lastChange = 0;
-        this.visible = true;
+    constructor(canvas, image, data, xPercentage, yPercentage) {
+        super(canvas, image, data, 0.0, 0.0);
+        this.platformX = xPercentage * GameEnv.innerWidth;
+        this.platformY = yPercentage;
+    }
+
+    size() {
+        // Formula for Height should be on constant ratio, using a proportion of 832
+        const scaledHeight = GameEnv.innerWidth * (1/27);
+        const scaledWidth = scaledHeight;  // width of jump platform is 1/10 of height
+        const platformX = this.platformX;
+        const platformY = (GameEnv.bottom - scaledHeight) * this.platformY;
+        // set variables used in Display and Collision algorithms
+        this.bottom = platformY;
+        this.collisionHeight = scaledHeight;
+        this.collisionWidth = scaledWidth;
+        //this.canvas.width = this.width;
+        //this.canvas.height = this.height;
+        this.canvas.style.width = `${scaledWidth}px`;
+        this.canvas.style.height = `${scaledHeight}px`;
+        this.canvas.style.position = 'absolute';
+        this.canvas.style.left = `${platformX}px`;
+        this.canvas.style.top = `${platformY}px`;
     }
 
     update() {
